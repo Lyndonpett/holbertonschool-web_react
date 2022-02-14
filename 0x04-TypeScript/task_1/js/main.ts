2f686fbe958e5cae4fc6e5da1d0adcfb684bbce5
lyndonpett <lyndonpett98@gmail.com>
2022-02-14 12:03:29 -0600

    Adding task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,25 @@ const printTeacher: printTeacherFunction = (
   firstName: string,
   lastName: string
 ) => `${firstName.substring(0, 1)}. ${lastName}`;
+
+interface StudentInterface {
+  firstName: string;
+  lastName: string;
+}
+
+interface StudentConstructor {
+  displayName: string;
+  workOnHomework: () => string;
+}
+
+class StudentClass implements StudentConstructor {
+  displayName: string;
+
+  constructor(args: StudentInterface) {
+    this.displayName = args.firstName;
+  }
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+}
