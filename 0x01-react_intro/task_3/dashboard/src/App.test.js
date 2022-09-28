e8b88ab307793b87ddb3cf4a3268a2d57f8e887d
lyndonpett <lyndonpett98@gmail.com>
2022-09-28 11:34:14 -0500

    Adding task 6

--- 0x01-react_intro/task_3/dashboard/src/App.test.js
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import * as ReactDOMClient from 'react-dom/client';
 import App from './App';
+import { shallow } from 'enzyme';
+import { assert } from 'chai';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    const root = ReactDOMClient.createRoot(div);
+    root.render(<App />);
+  });
+
+  it('renders the right header', () => {
+    const wrapper = shallow(<App />);
+    const header = wrapper.find('.App-header');
+    assert.equal(header.length, 1);
+  });
+
+  it('renders the right body', () => {
+    const wrapper = shallow(<App />);
+    const body = wrapper.find('.App-body');
+    assert.equal(body.length, 1);
+  });
+
+  it('renders the right footer', () => {
+    const wrapper = shallow(<App />);
+    const footer = wrapper.find('.App-footer');
+    assert.equal(footer.length, 1);
+  });
 });
