b8aa961859d2d6a6be5c06de8bc10e812c390233
lyndonpett <lyndonpett98@gmail.com>
2022-10-05 11:02:50 -0500

    Adding task 2

---
+++ 0x03-React_component/task_2/dashboard/src/Login/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Login.css';
+
+export default function Login() {
+  return (
+    <div id="Login">
+      <p>Login to access the full dashboard</p>
+      <form>
+        <label htmlFor="email">
+          Email:
+          <input type="email" id="email" name="email" />
+        </label>
+        <label htmlFor="password">
+          Password:
+          <input type="password" id="password" name="password" />
+        </label>
+        <button>OK</button>
+      </form>
+    </div>
+  );
+}
