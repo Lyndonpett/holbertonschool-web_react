3ac5bd3ce88756b4f8953c276253cd58c8c80a7a
lyndonpett <lyndonpett98@gmail.com>
2022-10-18 18:16:18 -0500

    ESLINT

--- 0x01-React_Immutable/7-equality.js
+++ 0x01-React_Immutable/7-equality.js
@@ -5,9 +5,9 @@ import { is } from 'immutable';
 // It accepts two arguments map1 and map2. Both are Immutable.js Maps
 // It should return true if the Maps are equal
 
-export const areMapsEqual = (map1, map2) => {
-    return is(map1, map2);
-};
+export default function areMapsEqual(map1, map2) {
+  return is(map1, map2);
+}
 
 // const map1 = Map({
 //     firstName: 'Guillaume',
