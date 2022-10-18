e2e74dbdb57b5d630766e5a877306d9cc115fa3e
lyndonpett <lyndonpett98@gmail.com>
2022-10-18 17:22:17 -0500

    Eslint

--- 0x01-React_Immutable/3-list.js
+++ 0x01-React_Immutable/3-list.js
@@ -1,15 +1,11 @@
 import { List } from 'immutable';
 
 // getListObject accepts an array as parameter and converts it into an immutable List using the Immutable.js library
-const getListObject = (arr) => {
-  return List(arr);
-};
+const getListObject = (arr) => List(arr);
 
 // addElementToList accepts two arguments: first one is a List and second one is a string
 // append the string to the list and return the list
 
-const addElementToList = (list, element) => {
-  return list.push(element);
-};
+const addElementToList = (list, element) => list.push(element);
 
 export { getListObject, addElementToList };
