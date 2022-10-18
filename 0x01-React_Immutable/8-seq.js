3730d34f3de08d80cd77707f6bf509074fbc0582
lyndonpett <lyndonpett98@gmail.com>
2022-10-18 18:17:06 -0500

    ESLINT

--- 0x01-React_Immutable/8-seq.js
+++ 0x01-React_Immutable/8-seq.js
@@ -1,17 +1,18 @@
 import { Seq } from 'immutable';
 
-// Calling the function should filter any student with a score < 70 and print to the console the first name and the last name with the first letter capitalized:
 export const printBestStudents = (grades) => {
-    const bestStudents = Seq(grades)
-        .filter((grade) => grade.score >= 70)
-        .map((grade) => {
-            return {
-                score: grade.score,
-                firstName: grade.firstName.charAt(0).toUpperCase() + grade.firstName.slice(1),
-                lastName: grade.lastName.charAt(0).toUpperCase() + grade.lastName.slice(1),
-            };
-        });
-    console.log(bestStudents.toJS());
+  const bestStudents = Seq(grades)
+    .filter((grade) => grade.score >= 70)
+    .map((grade) => {
+      return {
+        score: grade.score,
+        firstName:
+          grade.firstName.charAt(0).toUpperCase() + grade.firstName.slice(1),
+        lastName:
+          grade.lastName.charAt(0).toUpperCase() + grade.lastName.slice(1),
+      };
+    });
+  console.log(bestStudents.toJS());
 };
 
 // const grades = {
