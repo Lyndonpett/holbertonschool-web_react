4e36d9fee245c4a704a4161e002922ec6be2ef54
lyndonpett <lyndonpett98@gmail.com>
2022-10-18 10:24:19 -0500

    Adding task 6

---
+++ 0x01-React_Immutable/6-deeply.js
@@ -0,0 +1,34 @@
+import { Map } from 'immutable';
+
+// Create a function named mergeDeeplyElements
+// It should accept two arguments, page1 and page2. Both are objects
+// It should return a List containing the values of the two pages
+// If two values are the same, they should combine each other
+
+export const mergeDeeplyElements = (page1, page2) => {
+    return Map(page1).mergeDeep(Map(page2));
+};
+
+// const page1 = {
+//     'user-1': {
+//         id: 1,
+//         name: 'test',
+//         likes: {
+//             1: {
+//                 uid: 1234,
+//             },
+//         },
+//     },
+// };
+
+// const page2 = {
+//     'user-1': {
+//         likes: {
+//             2: {
+//                 uid: 134,
+//             },
+//         },
+//     },
+// };
+
+// console.log(mergeDeeplyElements(page1, page2).toJS());
