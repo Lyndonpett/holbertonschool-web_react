f81c7b492b19d68d1d02ad97c74e8977d9ebae62
lyndonpett <lyndonpett98@gmail.com>
2022-10-19 09:58:14 -0500

    Eslint

--- 0x01-React_Immutable/8-seq.js
+++ 0x01-React_Immutable/8-seq.js
@@ -1,20 +1,22 @@
 import { Seq } from 'immutable';
 
 export default function printBestStudents(grade) {
-	const students = {};
-	for (const student in grade) {
-	  if (grade[student].score >= 70) {
-		students[student] = {
-		  firstName: grade[student].firstName.charAt(0).toUpperCase()
-			+ grade[student].firstName.slice(1),
-		  lastName: grade[student].lastName.charAt(0).toUpperCase()
-			+ grade[student].lastName.slice(1),
-		  score: grade[student].score,
-		};
-	  }
-	}
-	console.log(Seq(students).toJS());
+  const students = {};
+  for (const student in grade) {
+    if (grade[student].score >= 70) {
+      students[student] = {
+        firstName:
+          grade[student].firstName.charAt(0).toUpperCase() +
+          grade[student].firstName.slice(1),
+        lastName:
+          grade[student].lastName.charAt(0).toUpperCase() +
+          grade[student].lastName.slice(1),
+        score: grade[student].score,
+      };
+    }
   }
+  console.log(Seq(students).toJS());
+}
 
 // const grades = {
 //     1: {
